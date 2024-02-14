1a276d07ee722486c6007bc29f52d6a53feec051
ysquareimperial <ysquare.theimperial@gmail.com>
2024-02-14 19:53:08 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import nexifourLogo from "../src/assets/nexifour.PNG";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank"></a>
-        <img src={nexifourLogo} className="logo" alt="Nexifour logo" />
+        <img
+          src="https://res.cloudinary.com/dx5ilizca/image/upload/v1707936770/nexifour_ddxxjc.png"
+          className="logo"
+          alt="Nexifour logo"
+        />
       </div>
       <h2>Nexifour LLC</h2>
       <div className="card">
