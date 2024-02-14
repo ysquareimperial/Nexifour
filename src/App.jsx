8deead22135ebb8c12e5269bead639892e74f3ab
ysquareimperial <ysquare.theimperial@gmail.com>
2024-02-14 19:25:12 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import nexifourLogo from "../src/assets/Nexifour.PNG";
+import nexifourLogo from "../src/assets/nexifour.PNG";
 import "./App.css";
 
 function App() {
